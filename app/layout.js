d814f0f8bc621c445a1b7c257c39027a0adb7639
fczoshi-ux <fcz.oshi@gmail.com>
2026-09-17 05:56:33 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Ali Store",
+  description: "Ali Store - Alight Motion Premium",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+    }
